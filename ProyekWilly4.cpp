53503fc3c5080a1abc6eb119b7bf7f6e53d162ea
william3012 <141609792+william3012@users.noreply.github.com>
2023-08-07 13:19:44 +0700

    Add files via upload

---
+++ ProyekWilly4.cpp
@@ -0,0 +1,29 @@
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+	//mendeklarasikan teks1 dan teks2
+	char teks1 [10], teks2[10];
+	int jlhdepan, jlhkel, jlhhuruf;
+	
+	//menampilkan pesan u/ meminta masukan dari pengguna
+	cout << "Masukkan Nama Depan Anda?:  ";
+	cin >> teks1;
+	cout << "Masukkan Nama Keluarga Anda?:  ";
+	cin >> teks2;
+	//menghitung panjang teks
+	jlhdepan = strlen (teks1);
+	jlhkel = strlen (teks2);
+	jlhhuruf = jlhdepan + jlhkel;
+	
+	//menapilkan hasil ke penguna
+cout << "Nama Lengkap Anda:   " <<  teks1 << " " << teks2 <<
+endl;
+cout << "Jumlah Huruf Nama Anda:    " << jlhhuruf << " Huruf"
+<<endl;
+  system ("PAUSE");
+  return 0;
+}
+
